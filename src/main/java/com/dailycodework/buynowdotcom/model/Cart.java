b4881b833642ac9c1298043be584f3273f39768c
ask-anuj <anuj.thakre01@gmail.com>
2025-10-30 22:46:12 +0530

    Project BE Entity Final Schema - Completed

--- src/main/java/com/dailycodework/buynowdotcom/model/Cart.java
+++ src/main/java/com/dailycodework/buynowdotcom/model/Cart.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -21,4 +23,7 @@ public class Cart {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>(); // One-to-many relationship with CartItem
 }
